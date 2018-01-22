fa5d6e8e65062710da33eb880a2b1f040bf17b66
James Nyamu <nyamujamesndungu@gmail.com>
2018-01-22 22:40:33 +0300

    add linting

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const builder = require('botbuilder')
 
 const connector = new builder.ConsoleConnector().listen()
 const bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text)
+	session.send('You said: %s', session.message.text)
 })
