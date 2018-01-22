775cf8dc9978fb44d82bd26e0cea3d1324ae2200
James Nyamu <nyamujamesndungu@gmail.com>
2018-01-22 22:21:07 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const builder = require('botbuilder')
+
+const connector = new builder.ConsoleConnector().listen()
+const bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text)
+})
