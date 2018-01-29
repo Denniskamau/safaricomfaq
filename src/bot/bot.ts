b5fccb34a8a7994516e7a0b0dfd689817a5e1b1e
James Nyamu <nyamujamesndungu@gmail.com>
2018-01-29 19:29:04 +0300

    WIP

---
+++ src/bot/bot.ts
@@ -0,0 +1,10 @@
+import * as builder from 'botbuilder'
+import * as axios from 'axios'
+
+export default class Bot {
+  constructor() {}
+
+  public async init() : void {
+
+  }
+}
--- src/controller/BotController.ts
+++ src/controller/BotController.ts
@@ -1,16 +1,20 @@
 import * as express from 'express'
 import * as builder from 'botbuilder'
+import * as axios from 'axios'
 import {injectable, inject} from 'inversify'
 
 import TYPES from '../types'
 
 import {RegistrableController} from './RegistrableController'
+import { hostname } from 'os';
 
 @injectable()
 export class BotController implements RegistrableController {
     private connector
     private bot
     private instructions = 'Welcome to the "Safom" bot. This will answer all FAQ questions from safaricom website'
+    private host = 'https://westus.api.cognitive.microsoft.com/qnamaker/v2.0'
+    private endpoint = '/knowledgebases/43b22a33-6cde-49de-98d8-66257bcd6356/generateAnswer'
 
     constructor() {
         this.connector = new builder.ChatConnector({
@@ -30,21 +34,21 @@ export class BotController implements RegistrableController {
             let name
             let email
             let phoneNumber
-            
-            if(text.indexOf('Name:') > -1 && text.indexOf('Email:') > -1) {
-                name = text.substring((text.lastIndexOf('Name:') + _name.length + 1), (text.indexOf('Email:') - 1)).replace(/\'/g,'')  
-            }
-            
-            if(text.indexOf('Email:') > -1 && text.indexOf('Phone Number:') > -1) {
-                email = text.substring((text.lastIndexOf('Email:') + _email.length + 1), (text.indexOf('Phone Number:') - 1)).replace(/\'/g,'')         
-            }
-            
-            if(text.indexOf('Phone Number:') > -1) {
-                phoneNumber = text.substring((text.lastIndexOf('Phone Number:') + _phoneNumber.length + 1)).replace(/\'/g,'')  
-            }
 
-            if(name && email && phoneNumber) {
-                reply.text(`Welcome ${name} - ${phoneNumber}. What inquiry do you have today?`)
+            if(text.indexOf('Name:') > -1 && text.indexOf('Email:') > -1 && text.indexOf('Phone Number:') > -1) {
+                name = text.substring((text.lastIndexOf('Name:') + _name.length + 1), (text.indexOf('Email:') - 1)).replace(/\'/g,'')  
+                email = text.substring((text.lastIndexOf('Email:') + _email.length + 1), (text.indexOf('Phone Number:') - 1)).replace(/\'/g,'')         
+                phoneNumber = text.substring((text.lastIndexOf('Phone Number:') + _phoneNumber.length + 1)).replace(/\'/g,'')
+                
+                reply.text(`Welcome ${name} - ${phoneNumber}. What inquiry do you have today? Respond with /Q:Some question here`)
+
+                session.send(reply)
+            } else if(text.indexOf('/Q:') > -1) {
+                const question = text.substring((text.lastIndexOf('/Q:') + 3))
+
+                const response = this.getResponse(question)
+
+                reply.text(`${response}. Are you satisfied with the answer?`)
 
                 session.send(reply)
             } else {
@@ -85,5 +89,22 @@ export class BotController implements RegistrableController {
                 })
             }
         })
+    }
+
+    private async getResponse(question: string): Promise<string> {
+        const authOptions = {
+            method: 'POST',
+            url: `${this.host}${this.endpoint}`,
+            data: question,
+            headers: {
+              'Content-Type': 'multipart/form-data',
+              'Ocp-Apim-Subscription-Key': '31366e4aba0d45818c2e8d4cb04b6909',
+            }
+          }
+      
+          const reply = await axios(authOptions)
+          console.log(reply)
+
+          return reply
     }
 }
