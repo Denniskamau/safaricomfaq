94601479042d431780a72583920c7df7c520c708
James Nyamu <nyamujamesndungu@gmail.com>
2018-01-29 10:46:31 +0300

    setup bot to respond through emulator

--- src/controller/BotController.ts
+++ src/controller/BotController.ts
@@ -9,15 +9,46 @@ import {RegistrableController} from './RegistrableController'
 @injectable()
 export class BotController implements RegistrableController {
     private connector
+    private bot
+    private instructions = 'Welcome to the Bot to showcase the DirectLine API. Send text to echo it.';
+
     constructor() {
         this.connector = new builder.ChatConnector({
             appId: process.env.MICROSOFT_APP_ID,
             appPassword: process.env.MICROSOFT_APP_PASSWORD
         })
+
+        this.bot = new builder.UniversalBot(this.connector, (session) => {
+            const reply = new builder.Message().address(session.message.address)
+
+            reply.text(`You said: ${session.message.text}`)
+
+            session.send(reply)
+        })
+
+        this.setupBot()
     }
 
     public register(app: express.Application): void {
         app.route('/api/messages')
             .post(this.connector.listen())
+    }
+
+    private setupBot(): void {
+        const _bot = this.bot
+        const _instructions = this.instructions
+        
+        this.bot.on('conversationUpdate',(activity) => {
+            if (activity.membersAdded) {
+                activity.membersAdded.forEach(function (identity) {
+                    if (identity.id === activity.address.bot.id) {
+                        var reply = new builder.Message()
+                            .address(activity.address)
+                            .text(_instructions)
+                        _bot.send(reply);
+                    }
+                });
+            }
+        })
     }
 }
