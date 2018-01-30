10148336a0f44a31c6f6a7792d094aade06bd729
James Nyamu <nyamujamesndungu@gmail.com>
2018-01-30 19:53:32 +0300

    Refactor for deployment

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,30 @@
+
 import * as express from 'express'
-import * as builder from 'botbuilder'
+import * as bodyParser from 'body-parser'
+import TYPES from './types'
+import container from './inversify.config'
+import { logger } from './util/Logger'
+import { RegistrableController } from './controller/RegistrableController'
 
-class App {
-  public express
-  private connector
+const app: express.Application = express()
 
-  constructor () {
-    this.express = express()
-    this.mountRoutes()
+app.use(bodyParser.json())
 
-    this.connector = new builder.ChatConnector({
-      appId: process.env.MICROSOFT_APP_ID,
-      appPassword: process.env.MICROSOFT_APP_PASSWORD
-    })
-  }
 
-  private mountRoutes (): void {
-    const router = express.Router()
-    
-    router.get('/', (request, response) => {
-      response.json({
-        message: 'pong'
-      })
-    })
-    
-    router.get('api/messages', (request, response) => {
-      response.json({
-        message: 'pong'
-      })
-    })
-    
-    this.express.use('/', router)
-  }
-}
+const controllers: RegistrableController[] = container.getAll<RegistrableController>(TYPES.Controller)
+controllers.forEach(controller => controller.register(app))
 
-export default new App().express
+app.use(function (err: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
+    logger.error(err.stack)
+    next(err)
+})
+
+app.use(function (err: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
+    res.status(500).send('Internal Server Error')
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, function () {
+    logger.info(`Safaricom FAQ App listening on port ${port}!`)
+})
